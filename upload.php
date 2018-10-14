4668707723a5e6aa3b31c1d7cfca00742fc2bdbd
AndresM77 <andres@triplean.com>
2018-10-14 09:53:55 -0400

    Edited buttons on topic page and posting methodology

--- upload.php
+++ upload.php
@@ -8,6 +8,7 @@ if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
+        // Method which transfers to a server 
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
